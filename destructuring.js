54d7dd67daf7937ce04fdc12bb81085641d96103
Matt <mjd014@live.co.uk>
2022-08-06 16:03:12 +0000

    added more destructuring examples.

--- destructuring.js
+++ destructuring.js
@@ -44,5 +44,23 @@ let languages2 = {
 let {firstLanguage, thirdLanguage} = languages2;
 console.log(firstLanguage, thirdLanguage);
 
-// Using rest parameter syntax
+// Using rest parameter syntax (...)
+// favourite, secondFavourite are the first and second items in the fruits array, respectively (apple, orange).
+// ...others is assigned the remaining items of fruits array.
+let fruits = ['apple', 'orange', 'banana', 'peach', 'cherry'];
+let [favourite, secondFavourite, ...others] = fruits;
+console.log(favourite);
+console.log(secondFavourite)
+console.log(others);
 
+// Object rest example, similar process as above but in an object.
+let favouriteFoods = {
+    brian: "pizza",
+    anna: "pasta",
+    sarah: "vegetarian",
+    andrea: "steak",
+};
+
+let { brian, anna, ...rest} = favouriteFoods;
+console.log(brian, anna);
+console.log(rest);
